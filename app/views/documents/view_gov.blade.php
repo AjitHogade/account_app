1a784f9a9e183600b4a4e6b312ee6607f981e1f9
Ajit <aju.20890@gmail.com>
2015-03-26 15:17:22 +0530

    solved modal erorr.mistake in jquery closing brackets at last of the script.

--- app/views/documents/view_gov.blade.php
+++ app/views/documents/view_gov.blade.php
@@ -2,7 +2,7 @@
 
 @extends('layout.basic')<br><br><br>
 @section('content')
-    {{Form::open(array('action'=>'DocumentController@insert','id'=>'contactform','file'=>true,'method' => 'post'))}}
+   
     <div class="row">
         <div class="row col-lg-3">
             @include('layout.accord')
@@ -47,6 +47,7 @@
 </td>
 
                     </tr>
+                </table>
             @endforeach
 
 
@@ -55,56 +56,41 @@
 
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="company-about" tabindex="-1" role="dialog" aria-labelledby="company-about-label" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="company-about-label">Company Name</h4>
-                </div>
-                <div class="modal-body">
-                    <p>Company summary...</p>
-                </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
 
-    <!-- Modal -->
-    <div class="modal fade" id="pancard_status_modal" tabindex="-1" role="dialog" aria-labelledby="fee-details-label" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="fee-details-label">Fee Details</h4>
-                </div>
-                <div class="modal-body">
-                    <table id="client_status_table" class="table table-striped table-hover view_entries_table">
 
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>City</th>
-                            <th>Status</th>
-                            <th>Message</th>
-                        </tr>
-                        </thead>
 
-                        <tbody>
 
-                        </tbody>
-                    </table>
-                </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
+<div class="modal fade" id="pancard_status_modal" tabindex="-1" role="dialog" aria-labelledby="fee-details-label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="fee-details-label">Fee Details</h4>
+            </div>
+            <div class="modal-body">
+                <table id="doc_status_table" class="table table-striped table-hover view_entries_table">
+
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>DOC Name</th>
+                     </tr>
+                    </thead>
+
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
     <script>
+
+
         function update(id,gov_doc_name) {
 
-            alert(id);
-            //alert(from);
-            //  alert(to);
-            //  return false;
             $.ajax({
 
                 type: "GET",
@@ -139,33 +125,23 @@
 
         }
 
-        function updatePancardModal(response,gov_doc_name ) {
-            alert(response);
-            var tableRows = '';
-            var data = response.length;
-            $("#pancard_status_modal").modal();
-                if(data!=""){
-                    alert(data);
-                    var result = "<table class='table'><tr><h3>Search Result</h3>";
-                    result += "<td>"+data.id+"</td>";
-                    result += '<td>"+data.gov_doc_name+"</td>';
-                    result += "</tr></table>";
-                    var result_div = $("#result");
-                    result_div.empty();
-                    result_div.append(result);
-                }else{
+ function updatePancardModal(response,gov_doc_name ) {
+var tableRows = '';
+var data_count = response.length;
+$("#pancard_status_modal").modal();
 
-                    alert("There is no such user");
-                }
-
-                    }
+if(data_count > 0){
+    $.each(response, function(i, item) {
+        tableRows += "<tr>";
+        tableRows += "<td>"+item.id+"</td>";
+        tableRows += "<td>"+item.gov_doc_name+"</td>";
+        tableRows += "</tr>";
+     });
+   $('#doc_status_table > tbody:last').empty();
+    $('#doc_status_table > tbody:last').append(tableRows);
+}else{
+alert("There is no such user");
+}
+}
     </script>
-    {{ Form::close() }}
-
-
-
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-
-
-
 @stop
